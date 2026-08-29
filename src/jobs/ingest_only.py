42980fec268099e16e468363c5bf5bc8de75ddf0
rytisgasparavicius <rytisgasparavicius@gmail.com>
2026-08-29 17:34:47 +0300

    a

--- src/jobs/ingest_only.py
+++ src/jobs/ingest_only.py
@@ -4,9 +4,9 @@ from pyspark.sql import functions as f
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArugmentParser()
-    parser.add_argument("--source-table",required=True)
-    parser.add_argument("--target-table",required=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source-table", required=True)
+    parser.add_argument("--target-table", required=True)
 
     return parser.parse_args()
 
